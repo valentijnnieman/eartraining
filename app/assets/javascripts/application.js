417c0ddc83a28b99fe27a256166458d32d4e1799
Chunkydory <valentijnnieman@gmail.com>
2016-10-16 14:52:57 +0200

    synth engine

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,6 @@
 // about supported directives.
 //
 //= require jquery
-//= require angular.min
 //= require jquery_ujs
 //= require foundation
 //= require vexflow-min
--- app/assets/javascripts/eartraining/snippet.js
+++ app/assets/javascripts/eartraining/snippet.js
@@ -1,28 +1,22 @@
 // Snippets are objects holding a single 'piece of music',
 // like an interval or a chord progression.
-//
 
 var Snippet = function(notes, element) {
-  this.notes = notes
-  this.element = element
+  this.notes = notes;
+  this.element = element;
+  this.synth = new Synth(this.notes.instrument); 
+
+  this.canvas = document.createElement('canvas')
+  this.renderer = new Vex.Flow.Renderer(this.canvas,
+    Vex.Flow.Renderer.Backends.CANVAS);
+  this.vexCtx = this.renderer.getContext();
 }
 
 Snippet.prototype.render = function(){
-  var canvas = document.createElement('canvas')
-  this.element.appendChild(canvas)
+  this.element.appendChild(this.canvas)
 
-  var renderer = new Vex.Flow.Renderer(canvas,
-    Vex.Flow.Renderer.Backends.CANVAS);
-
-  var vexCtx = renderer.getContext();
   var stave = new Vex.Flow.Stave(10, -18, 340);
-  console.log(this.notes.key)
-  stave.addClef("treble").addKeySignature(this.notes.key).setContext(vexCtx).draw();
-
-  // Here we need to decide what to render:
-  // All the notes?
-  // Only the root?
-  // Nothing?
+  stave.addClef("treble").addKeySignature(this.notes.key).setContext(this.vexCtx).draw();
 
   var voice = new Vex.Flow.Voice({
     num_beats: 2,
@@ -31,19 +25,29 @@ Snippet.prototype.render = function(){
   });
 
   var notesToDraw = []
+
   for (var i = 0; i < this.notes.all_notes.length; i++){
-    console.log(this.notes.all_notes[i].key)  
     note = new Vex.Flow.StaveNote({ clef: "treble", auto_stem: true, keys: [this.notes.all_notes[i].key], duration: this.notes.all_notes[i].duration }),
     notesToDraw.push(note)
   }
-    voice.addTickables(notesToDraw);
-    formatter = new Vex.Flow.Formatter().joinVoices([voice]).format([voice], 200);
-    voice.draw(vexCtx, stave);
-  
+
+  voice.addTickables(notesToDraw);
+  formatter = new Vex.Flow.Formatter().joinVoices([voice]).format([voice], 200);
+  voice.draw(this.vexCtx, stave);
 }
+
 Snippet.prototype.play = function(){
   // play the notes 
+  if (this.notes.type == 'interval') {
+    var that = this
+    this.synth.playNote(this.notes.all_notes[0].key, 0.01, 0.2, 1.0, 0.8); 
+    window.setTimeout(function() {
+      that.synth.playNote(that.notes.all_notes[1].key, 0.01, 0.2, 1.0, 0.8); 
+    }, this.notes.speed);
+  } 
 }
+
 Snippet.prototype.solve = function(){
   // handle right and wrong answers
 } 
+
