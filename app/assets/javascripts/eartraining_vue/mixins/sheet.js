509acd3c5519d478cb2fd27aabe822d176fd3b6f
Valentijn Nieman <valentijnnieman@gmail.com>
2017-04-05 16:49:34 +0200

    render sheet without notes

--- app/assets/javascripts/eartraining_vue/mixins/sheet.js
+++ app/assets/javascripts/eartraining_vue/mixins/sheet.js
@@ -35,6 +35,20 @@ var sheet = {
         this.$emit('show_notes')
       }
     }
+  },
+  mounted: function() {
+    this.renderer = new Vex.Flow.Renderer(this.$refs.canvas,
+      Vex.Flow.Renderer.Backends.CANVAS)
+    this.vexCtx = this.renderer.getContext()
+    var stave = new Vex.Flow.Stave(8, -16, 282);
+    stave.addClef("treble").addKeySignature(this.in_key_of).setContext(this.vexCtx).draw();
+
+    var voice = new Vex.Flow.Voice({
+      num_beats: 2,
+      beat_value: 4,
+      resolution: Vex.Flow.RESOLUTION
+    });
+    voice.draw(this.vexCtx, stave);
   }
 }
 
